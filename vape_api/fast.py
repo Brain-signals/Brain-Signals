47518964cb85235d811432f769b249cc57ffccef
pierre billon <pierre@billon.net>
2022-06-08 16:42:57 +0200

    Ajout du streamlit et de api

---
+++ vape_api/fast.py
@@ -0,0 +1,39 @@
+
+from datetime import datetime
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pandas import to_datetime
+import pandas as pd
+from fastapi import FastAPI, File, UploadFile
+from PIL import Image
+import io
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
+
+
+def read_imagefile(file) -> Image.Image:
+    image = Image.open(io.BytesIO(file))
+    return image
+
+
+@app.get("/")
+def root():
+    # YOUR CODE HERE
+    return {'greeting': 'Hello'}
+
+
+@app.post("/predict/image")
+def predict_api(file: UploadFile = File(...)):
+    extension = file.filename.split(".")[-1] in ("jpg", "jpeg", "png", "nii")
+    if not extension:
+        return "Image must be jpg or png format!"
+    image = read_imagefile(file.read())
+    return "merci"
