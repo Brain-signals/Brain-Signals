0da18cec08d6bfe803d8361df40f6bb66b94ee04
Adrien Combes <adriencombes.ac@gmail.com>
2022-06-24 23:58:59 +0200

    quick push before bed

--- app/app_v2.py
+++ app/app_v2.py
@@ -1,13 +1,53 @@
 import streamlit as st
 import requests
-from vape_model.utils import show_nii_3D
+from vape_model.utils import show_nii_3D, show_nii_2D
 from vape_model.files import NII_to_3Darray
+from PIL import Image
 
 st.set_page_config(
-    page_title="BrainSignal public API", page_icon="🧠", initial_sidebar_state="expanded"
+    page_title="BrainSignal public API",
+    page_icon="🧠",
+    initial_sidebar_state="expanded"
 )
 
 st.title('Welcome to BrainSignal public API')
+
+# image = Image.open('./Streamlit_folder/BrainSignal_.png')
+# st.image(image)
+
+st.header('Try our API')
+
+API_URL = 'https://vape-mri-image-rxbyapf3mq-ew.a.run.app/predict'
+
+volume_file = st.file_uploader(
+    label='👇 Drop your Nifti file here to access other functions',
+    type='nii',
+    help='Only .nii files are accepted yet')
+
+if volume_file is not None:
+
+    st.caption('Please choose an option :', unsafe_allow_html=False)
+    col1, col2, col3 = st.columns(3)
+
+    bt1 = col1.button('Show brain in 3D')
+    bt2 = col2.button('Show brain in 2D')
+    bt3 = col3.button('Make Prediction')
+
+    if bt1:
+        vol = NII_to_3Darray('app/sub-OAS30281_ses-d0042_T1w.nii')
+        st.write(show_nii_3D(vol))
+
+    if bt2:
+        vol = NII_to_3Darray('app/sub-OAS30281_ses-d0042_T1w.nii')
+        st.write(show_nii_2D(vol))
+
+    if bt3:
+        with st.spinner('Processing your Nifti file, please wait...'):
+            response = requests.post(API_URL, files={'nii_file': volume_file}).json()
+            print(response)
+        st.success(f'your prediction is : {response["pred"]}')
+
+st.header('About')
 
 with st.expander("What is BrainSignal ?"):
     """
@@ -17,35 +57,3 @@ with st.expander("What is BrainSignal ?"):
     detect Brain Pathologies from 3D MRI scans.
     We created a 3D convolutional neuronal networks ... bla bla bla.
     """
-
-st.header('Try our API')
-
-API_URL = 'https://vape-mri-image-rxbyapf3mq-ew.a.run.app/predict'
-
-volume_file = st.file_uploader(label='Drop your Nifti file',type='nii')
-
-use_example_file = st.checkbox(
-    'Or use example file', False, help='Use example file to demo the app'
-)
-
-if use_example_file:
-    volume_file = open('app/sub-OAS30281_ses-d0042_T1w.nii','rb')
-
-if volume_file is not None:
-    bt1 = st.button('Show brain in 3D')
-    bt2 = st.button('Show brain in 2D')
-    bt3 = st.button('Make Prediction')
-
-    if bt1:
-        vol = NII_to_3Darray('app/sub-OAS30281_ses-d0042_T1w.nii')
-        st.write(show_nii_3D(vol))
-
-    if bt2:
-        vol = NII_to_3Darray('app/sub-OAS30281_ses-d0042_T1w.nii')
-        st.write(show_nii_3D(vol))
-
-    if bt3:
-        with st.spinner('Processing your Nifti file, please wait...'):
-            response = requests.post(API_URL, files={'nii_file': volume_file}).json()
-            print(response)
-        st.success(f'your prediction is : {response["pred"]}')
--- vape_model/utils.py
+++ vape_model/utils.py
@@ -1,11 +1,14 @@
 import nibabel as nib
 import matplotlib.pyplot as plt
+import numpy as np
+from scipy import ndimage
+import plotly.graph_objects as go
 
 def NII_image_shape(path):
     test_load = nib.load(f'{path}').get_fdata()
     return test_load.shape
 
-def show_nii_3D(volume):
+def show_nii_2D(volume):
 
     slice_x = int(volume.shape[0]/2)
     slice_y = int(volume.shape[1]/2)
@@ -25,3 +28,86 @@ def show_nii_3D(volume):
     print(f'shape is {volume.shape}')
 
     return fig
+
+def show_nii_3D(volume):
+  volume = ndimage.zoom(volume,np.array([96/volume.shape[0], 96/volume.shape[1], 1]))
+
+  # Define frames
+  nb_frames = volume.shape[-1] - 1 #minus 1 frame for initial display
+
+  fig = go.Figure(frames=[go.Frame(data=go.Surface(z=(nb_frames/10 - k * 0.1) * np.ones((96, 96)),
+                                                   surfacecolor=np.rot90(volume[:,:,nb_frames - k]),
+                                                   cmin=0, cmax=255
+                                                   ),
+                                   name=str(k) # you need to name the frame for the animation to behave properly
+                                   )
+                  for k in range(20,nb_frames-10)])
+
+  # Add data to be displayed before animation starts
+  fig.add_trace(go.Surface(
+      z=nb_frames/10 * np.ones((96, 96)),
+      surfacecolor=np.rot90(volume[:,:,nb_frames]),
+      colorscale='Gray',
+      cmin=0, cmax=255,
+      colorbar=dict(thickness=20, ticklen=4)
+      ))
+
+
+  def frame_args(duration):
+      return {
+              "frame": {"duration": duration},
+              "mode": "immediate",
+              "fromcurrent": True,
+              "transition": {"duration": duration, "easing": "linear"},
+          }
+
+  sliders = [
+              {
+                  "pad": {"b": 10, "t": 60},
+                  "len": 0.9,
+                  "x": 0.1,
+                  "y": 0,
+                  "steps": [
+                      {
+                          "args": [[f.name], frame_args(0)],
+                          "label": str(k),
+                          "method": "animate",
+                      }
+                      for k, f in enumerate(fig.frames)
+                  ],
+              }
+          ]
+
+  # Layout
+  fig.update_layout(
+          title='Slices in volumetric data',
+          width=600,
+          height=600,
+          scene=dict(
+                      zaxis=dict(range=[0, nb_frames/10], autorange=False),
+                      aspectratio=dict(x=1, y=1, z=1),
+                      ),
+          updatemenus = [
+              {
+                  "buttons": [
+                      {
+                          "args": [None, frame_args(50)],
+                          "label": "&#9654;", # play symbol
+                          "method": "animate",
+                      },
+                      {
+                          "args": [[None], frame_args(0)],
+                          "label": "&#9724;", # pause symbol
+                          "method": "animate",
+                      },
+                  ],
+                  "direction": "left",
+                  "pad": {"r": 10, "t": 70},
+                  "type": "buttons",
+                  "x": 0.1,
+                  "y": 0,
+              }
+          ],
+          sliders=sliders
+    )
+  return fig
