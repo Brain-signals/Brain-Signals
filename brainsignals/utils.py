80fece314c63f89c3373cfda4f293ed3aea07f03
Adrien Combes <me@adriencombes.com>
2022-12-18 22:13:19 +0100

    added axis details on show_nii_2D function

--- brainsignals/utils.py
+++ brainsignals/utils.py
@@ -43,16 +43,16 @@ def show_nii_2D(volume):
 
     fig, (ax1, ax2, ax3) = plt.subplots(1,3,figsize=(10,4))
     ax1.imshow(volume[slice_x,:,:],cmap='gray')
-    ax1.set_ylabel('Y axis')
-    ax1.set_xlabel('Z axis')
+    ax1.set_ylabel('+++  Y axis  ---')
+    ax1.set_xlabel('---  Z axis  +++')
     ax1.tick_params(bottom=False,left=False,labelbottom=False,labelleft=False)
     ax2.imshow(volume[:,slice_y,:],cmap='gray')
-    ax2.set_ylabel('X axis')
-    ax2.set_xlabel('Z axis')
+    ax2.set_ylabel('+++  X axis  ---')
+    ax2.set_xlabel('---  Z axis  +++')
     ax2.tick_params(bottom=False,left=False,labelbottom=False,labelleft=False)
     ax3.imshow(volume[:,:,slice_z],cmap='gray')
-    ax3.set_ylabel('X axis')
-    ax3.set_xlabel('Y axis')
+    ax3.set_ylabel('+++  X axis  ---')
+    ax3.set_xlabel('---  Y axis  +++')
     ax3.tick_params(bottom=False,left=False,labelbottom=False,labelleft=False)
     return fig
 
